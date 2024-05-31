45c927587c59fcead7ac99671638975a1a18bfc1
cccchou <774228623@qq.com>
2024-05-31 20:50:57 +0800

    added readme.md

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # resages
+[![PyPI version](https://badge.fury.io/py/resages.svg)](https://badge.fury.io/py/resages)
 
+## Description
 resages is a package dedicated to the proper calculation of reservoir age offsets, particularly when dealing with pairs of radiocarbon (14C) and calendar ages. This package includes a set of radiocarbon calibration curves (IntCal13,  SHCal13，and IntCal20), a template folder, various examples, a manual, and three scripts written in python. To be precise, they are converted to python based on the Resage R language module(/ResAge_12_2015.zip)
 
 ## Installation
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
-
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 setup(
     name='resages',
-    version='0.2',
+    version='0.21',
     py_modules=['resages'],
     packages=find_packages(include=['resages', 'resages.*']),
     
@@ -18,7 +19,13 @@ setup(
         '': ['Inputdat/*'],  
     },
     include_package_data=True, 
-    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Alex_chou',
     author_email='alex_chou@sjtu.edu.com',
     description='Methods and codes for reservoir atmosphere 14C age offset calculations',
